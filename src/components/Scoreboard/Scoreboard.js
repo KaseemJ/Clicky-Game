26d7cd51d63b15e14ab8ee8f608a8d62a5b64422
wesley9mm <info.developer4@gmail.com>
2018-08-15 22:44:11 -0700

    add

--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -4,8 +4,8 @@ import "./Scoreboard.css";
 const Scoreboard = props => (
     <div className="jumbotron">
         <h1>Welcome to the sports game!</h1>
-        <p>team Matches: {props.matches}</p>
-        <p>team Guesses: {props.guesses}</p>
+        <p>Team Matches: {props.matches}</p>
+        <p>Team Guesses: {props.guesses}</p>
         <p>In this game you pick a team, hopefully they pick you too!</p>
     </div>
 );
